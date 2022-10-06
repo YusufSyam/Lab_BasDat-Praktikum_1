826c48978741e34af88c2b9982b56a422c7e561f
bshriyuni <bessesahriyuni@gmail.com>
2022-10-06 23:24:32 +0800

    Kumpul tugas minngu 3

---
+++ H071211008/Week 3/no1-2.sql
@@ -0,0 +1,18 @@
+#use classicmodels
+
+# 1. Menambahkan 3 orang employees baru pada tabel employees
+-- insert into employees 
+-- values
+-- (1334, 'Sahriyuni', 'Besse', 'x123', 'bessesahriyuni@gmail.com', 7, 1619, 'chef'),
+-- (1335, 'Maghfirah', 'Andi', 'x124', 'andimaghfirah@gmail.com', 7, 1619, 'chef'),
+-- (1336, 'Hilal', 'Adelia', 'x125', 'adeliapuspitahilal@gmail.com', 7, 1619, 'chef');
+
+# 2. Membuat sebuah kantor baru
+-- insert into offices
+-- values
+-- (8, 'Bali', '+623456789', 'jl.Menuju surga', 'btp', 'IND', 'Indonesia', 1234, 'IN');
+
+-- update employees
+-- set officeCode = 8
+-- where officeCode = 4 and jobTitle = "sales rep"
+
